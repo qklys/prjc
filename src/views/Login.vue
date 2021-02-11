69521a5e0455f2ad1e909fead4e96604f3771e1d
Филатов Сергей <modern443@gmail.com>
2021-02-11 18:22:57 +0300

    asdas

---
+++ src/views/Login.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<input v-model="login" type="text" placeholder="Login">
+		<input v-model="password" type="password" placeholder="Pass">
+		<input type="submit" placeholder="Join" v-on:clock="submit">
+	</div>
+</template>
+<script type="text/javascript">
+	import axios from 'axios'
+
+	export default{
+		data(){
+			return{
+				login: '',
+				password: '',
+
+			}
+
+		},
+		methods: {
+			axios.get('http://37.77.104.246/api/jsonstorage/?id=642d1ca9c540df262dcdd1df3f445cd5')
+				.then(res) =>{
+					let result = false;
+					let user;
+					{for(let i = 0; i<res.data.length; i++){
+						
+						if( this.login == res.data[i].login && this.password == res.data[i].password){
+							result = true;
+
+						}
+					}
+				}
+					
+				
+
+		
+
+
+</script>
